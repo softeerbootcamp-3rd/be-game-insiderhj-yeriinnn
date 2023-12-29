f5bbcf6f7c9c7b87ee981e1a2e60642ab8c77031
yeriinnn <angelrin7@naver.com>
2023-12-29 15:54:37 +0900

    [feat] 게임유틸 내용 변경

--- src/main/java/GameUtils.java
+++ src/main/java/GameUtils.java
@@ -17,7 +17,7 @@ public class GameUtils {
 
     // 주어진 플레이어와 뱅커의 값에 해당하는 행렬의 값을 반환하는 함수
     public static int findValue(int[][] matrix, Player player, Player banker) {
-        int playerValue = player.getTotalSum(); // 플레이어의 값 추출
+        int playerValue = player.getThirdCardValue(); // 플레이어의 값 추출
         int bankerValue = banker.getTotalSum(); // 뱅커의 값 추출
         return matrix[bankerValue][playerValue];
     }
