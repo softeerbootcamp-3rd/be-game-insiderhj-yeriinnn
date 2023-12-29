f3ff000a6e10310fd9575b200824f5faa457b444
yeriinnn <angelrin7@naver.com>
2023-12-29 14:43:37 +0900

    [add] 플레이어 클래스 생성

---
+++ src/main/java/Player.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Integer> cards;
+    private int totalSum;
+
+    public Player() {
+        this.cards = new ArrayList<>();
+        this.totalSum = 0;
+    }
+
+    public void receiveCard(int cardValue) {
+        cards.add(cardValue);
+        totalSum += cardValue;
+    }
+
+    public int getTotalSum() {
+        return totalSum;
+    }
+
+    public void showCards() {
+        System.out.println("플레이어의 카드 합: " + cards);
+    }
+}
+
