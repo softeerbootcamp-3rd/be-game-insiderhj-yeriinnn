102f697d241ab7b09a7b0530eec86e8a7d053520
insiderhj <insiderhj@gmail.com>
2023-12-29 15:55:53 +0900

    [add] gameUtils랑 합친 기능 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,126 +3,90 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int betting = 0;
+    private static int betting;
+    private static int seedMoney = 100;
+    private static int betMoney;
+    private static int tieBet;
 
     public static void main(String[] args) {
 
-        System.out.println("승리할 쪽에 배팅해주세요 - 1: 플레이어, 2: 뱅커");
-        String userInput = getInput();
-        if (userInput.equals("1")) betting = 1;
-        else if (userInput.equals("2")) betting = 2;
-        else throwError("올바른 입력 바랍니다~~");
+        System.out.println("바카라 게임에 어서오세용~");
 
-        int gameResult = playGame();
-        switch (gameResult) {
-            case 0:
-                System.out.println("플레이어 승!");
-                break;
-            case 1:
-                System.out.println("뱅커 승!");
-                break;
-            case 2:
-                System.out.println("비겼띠");
-                break;
-        }
-    }
+        while (seedMoney > 0) {
+            System.out.println("-----------게임시작-----------");
+            seedMoney -= 1;
+            betMoney = 1;
+            System.out.println("기본배팅 1코인이 차감되었습니다.");
+            showSeed();
 
-    // 플레이어 승: 0, 뱅커 승: 1
-    static int playGame() {
-        Player player = new Player();
-        Player banker = new Player();
+            betting = getUserInput("승리할 쪽에 배팅해주세요 - 1: 플레이어, 2: 뱅커", 1, 2);
+            betMoney += getUserInput("추가배팅할 금액을 입력해주세요 (원치 않을 시 0 입력)", 0, seedMoney);
+            seedMoney -= betMoney - 1;
+            tieBet = getUserInput("타이에 배팅할 금액을 입력해주세요 (원치 않을 시 0 입력)", 0, seedMoney);
+            seedMoney -= tieBet;
 
-        System.out.println("플레이어가 드로우 중입니다...");
-        player.receiveCard(drawCard());
-        player.receiveCard(drawCard());
-
-        System.out.println("뱅커가 드로우 중입니다...");
-        banker.receiveCard(drawCard());
-        banker.receiveCard(drawCard());
-
-        // 내추럴
-        if (player.getTotalSum() >= 8 || banker.getTotalSum() >= 9) {
-            System.out.println("카드의 합이 8 이상이므로 내추럴입니다.");
-            return gameResult(player, banker);
-        }
-
-        System.out.println("내추럴이 아니기 때문에 진행합니다.");
-        System.out.println("플레이어의 차례...");
-
-        if (player.getTotalSum() == 6 || player.getTotalSum() == 7) {
-            System.out.println("스탠드, 카드를 받지 않습니다.");
-            System.out.println("뱅커의 차례...");
-
-            if (banker.getTotalSum() == 6 || banker.getTotalSum() == 7) {
-                System.out.println("스탠드, 카드를 받지 않습니다.");
-            } else {
-                System.out.println("뱅커가 드로우 중입니다...");
-                banker.receiveCard(drawCard());
+            int gameResult = Game.playGame();
+            switch (gameResult) {
+                case 1:
+                    System.out.println("플레이어 승!");
+                    break;
+                case 2:
+                    System.out.println("뱅커 승!");
+                    break;
+                case 3:
+                    System.out.println("타이!");
+                    break;
             }
 
-        } else {
-
-            System.out.println("플레이어가 드로우 중입니다...");
-            player.receiveCard(drawCard());
-
-            System.out.println("뱅커의 차례...");
-//            if (GameUtils.bankerDraw() == 0) {
-            if (0 == 0) {
-                System.out.println("스탠드, 카드를 받지 않습니다.");
-            } else {
-                System.out.println("뱅커가 드로우 중입니다...");
-                banker.receiveCard(drawCard());
+            System.out.println("-----------정산타임-----------");
+            if (gameResult == 3) {
+                System.out.println("타이이기 때문에 플레이어와 뱅커에게 건 코인은 돌려드릴게요~");
+                seedMoney += betMoney;
+                if (tieBet > 0) {
+                    System.out.println("타이에 배팅하신 금액은 9배로 돌려드립니다!");
+                    seedMoney += tieBet * 9;
+                }
             }
+            else {
+                if (betting == gameResult) {
+                    System.out.println("게임에 승리해 " + betMoney + "코인을 획득하셨습니다. 굿");
+                    seedMoney += betMoney * 2;
+                } else {
+                    System.out.println("게임에 패배해 배팅하신 코인은 몰수됩니다~~");
+                }
+            }
+
+            showSeed();
+            if (seedMoney == 0) {
+                System.out.println("아이고 파산하셨군요~ 저런... 더이상 게임을 진행할 수 없습니다");
+                break;
+            }
+            int moreGame = getUserInput("더 하실건가요? - 1: ㅇㅇ, 2: ㄴㄴ", 1, 2);
+            if (moreGame == 2) break;
         }
 
-        return gameResult(player, banker);
+        System.out.println("안녕히 가세욤~");
     }
 
-
-    static String getInput() {
+    private static int getUserInput(String msg, int min, int max) {
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        scanner.close();
+        int userInput;
+
+        while (true) {
+            System.out.println(msg);
+            if (scanner.hasNextInt()) {
+                userInput = scanner.nextInt();
+                if (userInput >= min && userInput <= max)
+                    break;
+            }
+            System.out.println("올바른 숫자를 입력하세요!!!");
+            scanner.nextLine();
+        }
+
         return userInput;
     }
 
-    static int drawCard() {
-        System.out.print("결과: ");
-        int result = getInt();
-        switch (result) {
-            case 11:
-                System.out.println("J");
-                return 0;
-            case 12:
-                System.out.println("Q");
-                return 0;
-            case 13:
-                System.out.println("K");
-                return 0;
-            default:
-                System.out.println(result);
-        }
-        return result;
-    }
-
-    static int getInt() {
-        Random random = new Random();
-        return random.nextInt(13) + 1;
-    }
-
-    static int gameResult(Player player, Player banker) {
-        System.out.println("플레이어의 숫자 합: " + player.getTotalSum());
-        System.out.println("뱅커의 숫자 합: " + banker.getTotalSum());
-
-        if (player.getTotalSum() > banker.getTotalSum()) {
-            return 0;
-        } else if (player.getTotalSum() < banker.getTotalSum()) {
-            return 1;
-        }
-        return 2;
-    }
-
-    static void throwError(String s) {
-        throw new IllegalArgumentException(s);
+    static void showSeed() {
+        System.out.println("현재 시드: " + seedMoney + "코인");
     }
 }
